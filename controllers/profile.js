f3f35e2cb154f626354ff089e752f2e18a26ae02
YouJia Shhi <shi.shiyoujia@gmail.com>
2019-07-23 00:00:38 -0700

    cc

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,7 +1,7 @@
 const handleProfileGet = (req, res, db) => {
     const { id } = req.params;
-    console.log('db is what???????', db);
-    db.select('*').from('users').where({ id })
+    console.log('db is what???????', req.params);
+    db.select('*').from('login').where({ id })
         .then(user => {
             if (user.length) {
                 res.json(user[0])
